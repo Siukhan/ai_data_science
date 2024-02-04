ce3231e3e2cab3028dbf017acb897aaf395fcafb
Sheikh Irfan Ullah Khan <149785627+Siukhan@users.noreply.github.com>
2024-02-05 04:00:16 +0500

    Add files via upload
    How to define operators in python

---
+++ 01_operators.py
@@ -0,0 +1,21 @@
+print("We are learning python with codanics")
+# Syntax is a language we do use to work in that envoirnment
+print("Irfan") # This data type is known as string(str)
+print('COMSATS')
+print(''' Irfan
+      COMSATS''')
+
+# Operators
+print(2+3) # This data type is integer (int)
+print(3.5 + 2.5) # This data type is integer (float)
+
+a = [1,2,3]
+b = [4,5,6]
+c = [4,5,6]
+
+print(a)
+print(b)
+print(a + b)
+                # True False, Yes No, 0 1 are Bollean Operators
+print(a == b)
+print(b == c)
